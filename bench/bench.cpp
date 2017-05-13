14fced009e6f8a6bc11b49ffeb4628691a0d1672
Anton Autushka <antoninus.liberalis@gmail.com>
2017-05-13 03:05:01 +0000

    bech m7 machine

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -441,6 +441,106 @@ struct bs : sc::simple_state<bs, active>
 
 using machine6 = v6::machine;
 
+namespace v7
+{
+
+template <int ... I>
+struct int_list;
+
+template <int I, int ... Is>
+struct int_list<I, Is ...> 
+{
+	enum {head = I};
+	using tail = int_list<Is ...>;
+};
+
+template <>
+struct int_list<>
+{
+};
+
+enum states {A, B};
+
+class machine
+{
+public:
+	template <int N>
+	void process_state(char ch);
+
+	void transition(int state)
+	{
+		state_ = state;
+	}
+
+	void send(char ch)
+	{
+		react(ch);
+	}
+
+	void react(char ch);
+
+private:
+	volatile int as_ = 0;
+	volatile int bs_ = 0;
+	int state_ = A;
+};
+
+template <>
+void machine::process_state<A>(char ch)
+{
+	if (ch == 'a')
+	{
+		++as_;
+	}
+	else
+	{
+		transition(B);
+		send(ch);
+	}
+}
+
+template <>
+void machine::process_state<B>(char ch)
+{
+
+	if (ch == 'b')
+	{
+		++bs_;
+	}
+	else
+	{
+		transition(A);
+		send(ch);
+	}
+}
+
+template <typename States>
+void call_state(int state, machine& m, char ch)
+{
+	if (state == States::head)
+	{
+		m.process_state<States::head>(ch);
+	}
+	else
+	{
+		call_state<typename States::tail>(state, m, ch);
+	}
+}
+
+template <>
+void call_state<int_list<>>(int state, machine& m, char ch)
+{
+}
+
+void machine::react(char ch)
+{
+	call_state<int_list<A, B>>(state_, *this, ch);
+}
+
+};
+
+using machine7 = v7::machine;
+
 template <class T> void
 state_machine(benchmark::State& state) 
 {
@@ -467,5 +567,6 @@ BENCHMARK_TEMPLATE(state_machine, machine3);
 BENCHMARK_TEMPLATE(state_machine, machine4);
 BENCHMARK_TEMPLATE(state_machine, machine5);
 BENCHMARK_TEMPLATE(state_machine, machine6);
+BENCHMARK_TEMPLATE(state_machine, machine7);
 
 BENCHMARK_MAIN();
