fdea12918b7985a4de1148a0d0b87c5e3e3fdf60
Anton Autushka <antoninus.liberalis@gmail.com>
2017-05-16 02:17:09 +0000

    fixed the m1 state-machine for better performance

--- rave/m1.h
+++ rave/m1.h
@@ -7,32 +7,42 @@ class machine
 public:
 	enum states { A, B};
 
-	void react(char ch)
+	void process_a()
 	{
 		switch (state_)
 		{
-			case A: 
-				if (ch == 'a')
-				{
-					++as_;
-				}
-				else 
-				{
-					state_ = B;
-					react(ch);
-				}
-				break;
-			case B:
-				if (ch == 'b')
-				{
-					++bs_;
-				}
-				else
-				{
-					state_ = A;
-					react(ch);
-				}
+		case A:
+			++as_;
+			break;
+		case B:
+			state_ = A;
+			++bs_;
 		};
+	}
+
+	void process_b()
+	{
+		switch (state_)
+		{
+		case A:
+			state_ = B;
+			++as_;
+		case B:
+			++bs_;
+			break;
+		};
+	}
+
+	void react(char ch)
+	{
+		if (ch == 'a')
+		{
+			process_a();
+		}
+		else
+		{
+			process_b();
+		}
 	}
 
 private:
