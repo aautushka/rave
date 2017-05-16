4249dca01a169193200306d0ca619ceef492fb72
Anton Autushka <antoninus.liberalis@gmail.com>
2017-05-16 04:11:18 +0000

    ability for rave2 to handle up to 15 states

--- rave/rave2.h
+++ rave/rave2.h
@@ -84,6 +84,7 @@ struct dispatcher<2, T> : public T
 		};
 	};
 };
+
 template <typename T>
 struct dispatcher<3, T> : public T
 {
@@ -105,6 +106,632 @@ struct dispatcher<3, T> : public T
 		case 2:
 			using state2 = typename meta::get_type<2, T>::type;
 			this->state2::react(event);
+			break;
+		};
+	}
+};
+
+template <typename T>
+struct dispatcher<4, T> : public T
+{
+	static_assert(meta::get_size<T>::value == 4, "");
+
+	template <typename Event>
+	void dispatch(int state, Event event)
+	{
+		switch (state)
+		{
+		case 0:
+			using state0 = typename meta::get_type<0, T>::type;
+			this->state0::react(event);
+			break;
+		case 1:
+			using state1 = typename meta::get_type<1, T>::type;
+			this->state1::react(event);
+			break;
+		case 2:
+			using state2 = typename meta::get_type<2, T>::type;
+			this->state2::react(event);
+			break;
+		case 3:
+			using state3 = typename meta::get_type<3, T>::type;
+			this->state3::react(event);
+			break;
+		};
+	}
+};
+
+
+template <typename T>
+struct dispatcher<5, T> : public T
+{
+	static_assert(meta::get_size<T>::value == 5, "");
+
+	template <typename Event>
+	void dispatch(int state, Event event)
+	{
+		switch (state)
+		{
+		case 0:
+			using state0 = typename meta::get_type<0, T>::type;
+			this->state0::react(event);
+			break;
+		case 1:
+			using state1 = typename meta::get_type<1, T>::type;
+			this->state1::react(event);
+			break;
+		case 2:
+			using state2 = typename meta::get_type<2, T>::type;
+			this->state2::react(event);
+			break;
+		case 3:
+			using state3 = typename meta::get_type<3, T>::type;
+			this->state3::react(event);
+			break;
+		case 4:
+			using state4 = typename meta::get_type<4, T>::type;
+			this->state4::react(event);
+			break;
+		};
+	}
+};
+
+template <typename T>
+struct dispatcher<6, T> : public T
+{
+	static_assert(meta::get_size<T>::value == 6, "");
+
+	template <typename Event>
+	void dispatch(int state, Event event)
+	{
+		switch (state)
+		{
+		case 0:
+			using state0 = typename meta::get_type<0, T>::type;
+			this->state0::react(event);
+			break;
+		case 1:
+			using state1 = typename meta::get_type<1, T>::type;
+			this->state1::react(event);
+			break;
+		case 2:
+			using state2 = typename meta::get_type<2, T>::type;
+			this->state2::react(event);
+			break;
+		case 3:
+			using state3 = typename meta::get_type<3, T>::type;
+			this->state3::react(event);
+			break;
+		case 4:
+			using state4 = typename meta::get_type<4, T>::type;
+			this->state4::react(event);
+			break;
+		case 5:
+			using state5 = typename meta::get_type<5, T>::type;
+			this->state5::react(event);
+			break;
+		};
+	}
+};
+
+template <typename T>
+struct dispatcher<7, T> : public T
+{
+	static_assert(meta::get_size<T>::value == 7, "");
+
+	template <typename Event>
+	void dispatch(int state, Event event)
+	{
+		switch (state)
+		{
+		case 0:
+			using state0 = typename meta::get_type<0, T>::type;
+			this->state0::react(event);
+			break;
+		case 1:
+			using state1 = typename meta::get_type<1, T>::type;
+			this->state1::react(event);
+			break;
+		case 2:
+			using state2 = typename meta::get_type<2, T>::type;
+			this->state2::react(event);
+			break;
+		case 3:
+			using state3 = typename meta::get_type<3, T>::type;
+			this->state3::react(event);
+			break;
+		case 4:
+			using state4 = typename meta::get_type<4, T>::type;
+			this->state4::react(event);
+			break;
+		case 5:
+			using state5 = typename meta::get_type<5, T>::type;
+			this->state5::react(event);
+			break;
+		case 6:
+			using state6 = typename meta::get_type<6, T>::type;
+			this->state6::react(event);
+			break;
+		};
+	}
+};
+
+template <typename T>
+struct dispatcher<8, T> : public T
+{
+	static_assert(meta::get_size<T>::value == 7, "");
+
+	template <typename Event>
+	void dispatch(int state, Event event)
+	{
+		switch (state)
+		{
+		case 0:
+			using state0 = typename meta::get_type<0, T>::type;
+			this->state0::react(event);
+			break;
+		case 1:
+			using state1 = typename meta::get_type<1, T>::type;
+			this->state1::react(event);
+			break;
+		case 2:
+			using state2 = typename meta::get_type<2, T>::type;
+			this->state2::react(event);
+			break;
+		case 3:
+			using state3 = typename meta::get_type<3, T>::type;
+			this->state3::react(event);
+			break;
+		case 4:
+			using state4 = typename meta::get_type<4, T>::type;
+			this->state4::react(event);
+			break;
+		case 5:
+			using state5 = typename meta::get_type<5, T>::type;
+			this->state5::react(event);
+			break;
+		case 6:
+			using state6 = typename meta::get_type<6, T>::type;
+			this->state6::react(event);
+			break;
+		case 7:
+			using state7 = typename meta::get_type<7, T>::type;
+			this->state7::react(event);
+			break;
+		};
+	}
+};
+
+template <typename T>
+struct dispatcher<9, T> : public T
+{
+	static_assert(meta::get_size<T>::value == 9, "");
+
+	template <typename Event>
+	void dispatch(int state, Event event)
+	{
+		switch (state)
+		{
+		case 0:
+			using state0 = typename meta::get_type<0, T>::type;
+			this->state0::react(event);
+			break;
+		case 1:
+			using state1 = typename meta::get_type<1, T>::type;
+			this->state1::react(event);
+			break;
+		case 2:
+			using state2 = typename meta::get_type<2, T>::type;
+			this->state2::react(event);
+			break;
+		case 3:
+			using state3 = typename meta::get_type<3, T>::type;
+			this->state3::react(event);
+			break;
+		case 4:
+			using state4 = typename meta::get_type<4, T>::type;
+			this->state4::react(event);
+			break;
+		case 5:
+			using state5 = typename meta::get_type<5, T>::type;
+			this->state5::react(event);
+			break;
+		case 6:
+			using state6 = typename meta::get_type<6, T>::type;
+			this->state6::react(event);
+			break;
+		case 7:
+			using state7 = typename meta::get_type<7, T>::type;
+			this->state7::react(event);
+			break;
+		case 8:
+			using state8 = typename meta::get_type<8, T>::type;
+			this->state8::react(event);
+			break;
+		};
+	}
+};
+
+
+template <typename T>
+struct dispatcher<10, T> : public T
+{
+	static_assert(meta::get_size<T>::value == 10, "");
+
+	template <typename Event>
+	void dispatch(int state, Event event)
+	{
+		switch (state)
+		{
+		case 0:
+			using state0 = typename meta::get_type<0, T>::type;
+			this->state0::react(event);
+			break;
+		case 1:
+			using state1 = typename meta::get_type<1, T>::type;
+			this->state1::react(event);
+			break;
+		case 2:
+			using state2 = typename meta::get_type<2, T>::type;
+			this->state2::react(event);
+			break;
+		case 3:
+			using state3 = typename meta::get_type<3, T>::type;
+			this->state3::react(event);
+			break;
+		case 4:
+			using state4 = typename meta::get_type<4, T>::type;
+			this->state4::react(event);
+			break;
+		case 5:
+			using state5 = typename meta::get_type<5, T>::type;
+			this->state5::react(event);
+			break;
+		case 6:
+			using state6 = typename meta::get_type<6, T>::type;
+			this->state6::react(event);
+			break;
+		case 7:
+			using state7 = typename meta::get_type<7, T>::type;
+			this->state7::react(event);
+			break;
+		case 8:
+			using state8 = typename meta::get_type<8, T>::type;
+			this->state8::react(event);
+			break;
+		case 9:
+			using state9 = typename meta::get_type<9, T>::type;
+			this->state9::react(event);
+			break;
+		};
+	}
+};
+
+template <typename T>
+struct dispatcher<11, T> : public T
+{
+	static_assert(meta::get_size<T>::value == 11, "");
+
+	template <typename Event>
+	void dispatch(int state, Event event)
+	{
+		switch (state)
+		{
+		case 0:
+			using state0 = typename meta::get_type<0, T>::type;
+			this->state0::react(event);
+			break;
+		case 1:
+			using state1 = typename meta::get_type<1, T>::type;
+			this->state1::react(event);
+			break;
+		case 2:
+			using state2 = typename meta::get_type<2, T>::type;
+			this->state2::react(event);
+			break;
+		case 3:
+			using state3 = typename meta::get_type<3, T>::type;
+			this->state3::react(event);
+			break;
+		case 4:
+			using state4 = typename meta::get_type<4, T>::type;
+			this->state4::react(event);
+			break;
+		case 5:
+			using state5 = typename meta::get_type<5, T>::type;
+			this->state5::react(event);
+			break;
+		case 6:
+			using state6 = typename meta::get_type<6, T>::type;
+			this->state6::react(event);
+			break;
+		case 7:
+			using state7 = typename meta::get_type<7, T>::type;
+			this->state7::react(event);
+			break;
+		case 8:
+			using state8 = typename meta::get_type<8, T>::type;
+			this->state8::react(event);
+			break;
+		case 9:
+			using state9 = typename meta::get_type<9, T>::type;
+			this->state9::react(event);
+			break;
+		case 10:
+			using state10 = typename meta::get_type<10, T>::type;
+			this->state10::react(event);
+			break;
+		};
+	}
+};
+
+template <typename T>
+struct dispatcher<12, T> : public T
+{
+	static_assert(meta::get_size<T>::value == 12, "");
+
+	template <typename Event>
+	void dispatch(int state, Event event)
+	{
+		switch (state)
+		{
+		case 0:
+			using state0 = typename meta::get_type<0, T>::type;
+			this->state0::react(event);
+			break;
+		case 1:
+			using state1 = typename meta::get_type<1, T>::type;
+			this->state1::react(event);
+			break;
+		case 2:
+			using state2 = typename meta::get_type<2, T>::type;
+			this->state2::react(event);
+			break;
+		case 3:
+			using state3 = typename meta::get_type<3, T>::type;
+			this->state3::react(event);
+			break;
+		case 4:
+			using state4 = typename meta::get_type<4, T>::type;
+			this->state4::react(event);
+			break;
+		case 5:
+			using state5 = typename meta::get_type<5, T>::type;
+			this->state5::react(event);
+			break;
+		case 6:
+			using state6 = typename meta::get_type<6, T>::type;
+			this->state6::react(event);
+			break;
+		case 7:
+			using state7 = typename meta::get_type<7, T>::type;
+			this->state7::react(event);
+			break;
+		case 8:
+			using state8 = typename meta::get_type<8, T>::type;
+			this->state8::react(event);
+			break;
+		case 9:
+			using state9 = typename meta::get_type<9, T>::type;
+			this->state9::react(event);
+			break;
+		case 10:
+			using state10 = typename meta::get_type<10, T>::type;
+			this->state10::react(event);
+			break;
+		case 11:
+			using state11 = typename meta::get_type<11, T>::type;
+			this->state11::react(event);
+			break;
+		};
+	}
+};
+
+template <typename T>
+struct dispatcher<13, T> : public T
+{
+	static_assert(meta::get_size<T>::value == 13, "");
+
+	template <typename Event>
+	void dispatch(int state, Event event)
+	{
+		switch (state)
+		{
+		case 0:
+			using state0 = typename meta::get_type<0, T>::type;
+			this->state0::react(event);
+			break;
+		case 1:
+			using state1 = typename meta::get_type<1, T>::type;
+			this->state1::react(event);
+			break;
+		case 2:
+			using state2 = typename meta::get_type<2, T>::type;
+			this->state2::react(event);
+			break;
+		case 3:
+			using state3 = typename meta::get_type<3, T>::type;
+			this->state3::react(event);
+			break;
+		case 4:
+			using state4 = typename meta::get_type<4, T>::type;
+			this->state4::react(event);
+			break;
+		case 5:
+			using state5 = typename meta::get_type<5, T>::type;
+			this->state5::react(event);
+			break;
+		case 6:
+			using state6 = typename meta::get_type<6, T>::type;
+			this->state6::react(event);
+			break;
+		case 7:
+			using state7 = typename meta::get_type<7, T>::type;
+			this->state7::react(event);
+			break;
+		case 8:
+			using state8 = typename meta::get_type<8, T>::type;
+			this->state8::react(event);
+			break;
+		case 9:
+			using state9 = typename meta::get_type<9, T>::type;
+			this->state9::react(event);
+			break;
+		case 10:
+			using state10 = typename meta::get_type<10, T>::type;
+			this->state10::react(event);
+			break;
+		case 11:
+			using state11 = typename meta::get_type<11, T>::type;
+			this->state11::react(event);
+			break;
+		case 12:
+			using state12 = typename meta::get_type<12, T>::type;
+			this->state12::react(event);
+			break;
+		};
+	}
+};
+
+template <typename T>
+struct dispatcher<14, T> : public T
+{
+	static_assert(meta::get_size<T>::value == 14, "");
+
+	template <typename Event>
+	void dispatch(int state, Event event)
+	{
+		switch (state)
+		{
+		case 0:
+			using state0 = typename meta::get_type<0, T>::type;
+			this->state0::react(event);
+			break;
+		case 1:
+			using state1 = typename meta::get_type<1, T>::type;
+			this->state1::react(event);
+			break;
+		case 2:
+			using state2 = typename meta::get_type<2, T>::type;
+			this->state2::react(event);
+			break;
+		case 3:
+			using state3 = typename meta::get_type<3, T>::type;
+			this->state3::react(event);
+			break;
+		case 4:
+			using state4 = typename meta::get_type<4, T>::type;
+			this->state4::react(event);
+			break;
+		case 5:
+			using state5 = typename meta::get_type<5, T>::type;
+			this->state5::react(event);
+			break;
+		case 6:
+			using state6 = typename meta::get_type<6, T>::type;
+			this->state6::react(event);
+			break;
+		case 7:
+			using state7 = typename meta::get_type<7, T>::type;
+			this->state7::react(event);
+			break;
+		case 8:
+			using state8 = typename meta::get_type<8, T>::type;
+			this->state8::react(event);
+			break;
+		case 9:
+			using state9 = typename meta::get_type<9, T>::type;
+			this->state9::react(event);
+			break;
+		case 10:
+			using state10 = typename meta::get_type<10, T>::type;
+			this->state10::react(event);
+			break;
+		case 11:
+			using state11 = typename meta::get_type<11, T>::type;
+			this->state11::react(event);
+			break;
+		case 12:
+			using state12 = typename meta::get_type<12, T>::type;
+			this->state12::react(event);
+			break;
+		case 13:
+			using state13 = typename meta::get_type<13, T>::type;
+			this->state13::react(event);
+			break;
+		};
+	}
+};
+
+template <typename T>
+struct dispatcher<15, T> : public T
+{
+	static_assert(meta::get_size<T>::value == 15, "");
+
+	template <typename Event>
+	void dispatch(int state, Event event)
+	{
+		switch (state)
+		{
+		case 0:
+			using state0 = typename meta::get_type<0, T>::type;
+			this->state0::react(event);
+			break;
+		case 1:
+			using state1 = typename meta::get_type<1, T>::type;
+			this->state1::react(event);
+			break;
+		case 2:
+			using state2 = typename meta::get_type<2, T>::type;
+			this->state2::react(event);
+			break;
+		case 3:
+			using state3 = typename meta::get_type<3, T>::type;
+			this->state3::react(event);
+			break;
+		case 4:
+			using state4 = typename meta::get_type<4, T>::type;
+			this->state4::react(event);
+			break;
+		case 5:
+			using state5 = typename meta::get_type<5, T>::type;
+			this->state5::react(event);
+			break;
+		case 6:
+			using state6 = typename meta::get_type<6, T>::type;
+			this->state6::react(event);
+			break;
+		case 7:
+			using state7 = typename meta::get_type<7, T>::type;
+			this->state7::react(event);
+			break;
+		case 8:
+			using state8 = typename meta::get_type<8, T>::type;
+			this->state8::react(event);
+			break;
+		case 9:
+			using state9 = typename meta::get_type<9, T>::type;
+			this->state9::react(event);
+			break;
+		case 10:
+			using state10 = typename meta::get_type<10, T>::type;
+			this->state10::react(event);
+			break;
+		case 11:
+			using state11 = typename meta::get_type<11, T>::type;
+			this->state11::react(event);
+			break;
+		case 12:
+			using state12 = typename meta::get_type<12, T>::type;
+			this->state12::react(event);
+			break;
+		case 13:
+			using state13 = typename meta::get_type<13, T>::type;
+			this->state13::react(event);
+			break;
+		case 14:
+			using state14 = typename meta::get_type<14, T>::type;
+			this->state14::react(event);
 			break;
 		};
 	}
